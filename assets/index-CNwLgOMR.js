510b609564d3150b2f1d1a53e0cacaf793ee0a55
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-04-17 16:49:37 -0300

    Updates

--- assets/index-CNwLgOMR.js
+++ assets/index-CNwLgOMR.js
@@ -1,4 +1,4 @@
-import{r as t,j as e,M as i,c as r,g as n,u as s}from"./index-C6VILv3N.js";var a,o={exports:{}};
+import{r as t,j as e,M as i,c as r,g as n,u as s}from"./index-BHxmJBvf.js";var a,o={exports:{}};
 /**
  * MapLibre GL JS
  * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v4.7.1/LICENSE.txt
