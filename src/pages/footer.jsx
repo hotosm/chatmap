cc156c953eea2f0a02804342e2c2f0bae098a157
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-06-24 13:05:49 -0300

    Remove unused component, improve styling, remove GH icon

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -3,13 +3,14 @@ export default function Footer() {
   return (
     <>
     <p className="info">
-        <strong>From chat to map</strong> check this quick <strong>
-        <a href="https://www.youtube.com/watch?v=ScHgVhyj1aw">video tutorial</a></strong>
+        <strong>From chat to map</strong> check this quick <strong><a href="https://www.youtube.com/watch?v=ScHgVhyj1aw">video tutorial</a></strong>
+    </p>
+    <p className="info">
+       Save your downloaded map in <a href="https://umap.hotosm.org/chatmap">umap.hotosm.org/chatmap</a>
     </p>
     <div className="infoLinks">
         <div className="copy">
-            <a className="github" href="https://github.com/hotosm/chatmap"></a>
-            <a href="https://github.com/hotosm/chatmap">Free Software</a>
+            <a href="https://github.com/hotosm/chatmap">This is free software</a>
         </div>
         <a href="https://www.hotosm.org/privacy">- We collect zero personal data. hotosm.org/privacy -</a>
         &nbsp;
