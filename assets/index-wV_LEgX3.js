1129e0af7c1906ce2b20e6ac3d45ca42990e4059
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-08-03 20:13:46 -0300

    Updates

--- assets/index-wV_LEgX3.js
+++ assets/index-wV_LEgX3.js
@@ -1,4 +1,4 @@
-import{r as t,j as e,M as i,c as r,g as n,u as s}from"./index-BdxZJOP1.js";var a,o={exports:{}};
+import{r as t,j as e,M as i,c as r,g as n,u as s}from"./index-BxihLgpe.js";var a,o={exports:{}};
 /**
  * MapLibre GL JS
  * @license 3-Clause BSD. Full text of license: https://github.com/maplibre/maplibre-gl-js/blob/v4.7.1/LICENSE.txt
