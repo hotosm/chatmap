9d4679acac19e5c40c594b783ce306e6113717d1
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-09-22 12:29:05 -0300

    Remove confirmation when new file is clicked

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -80,6 +80,7 @@ function App() {
     mapDataDispatch({
       type: 'reset'
     })
+    window.removeEventListener('beforeunload', confirmPageLeave);
   }
 
   // Handle uploaded files error (ex: invalid chat export)
