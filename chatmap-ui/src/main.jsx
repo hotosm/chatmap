9089b2417fe40bd2f2f792fa8670ede87f64e46e
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2026-01-16 23:01:24 -0300

    - Remove unused Shoelace components

--- chatmap-ui/src/main.jsx
+++ chatmap-ui/src/main.jsx
@@ -21,10 +21,8 @@ import { MapDataProvider } from './context/MapDataContext';
 
 // Shoelace UI components
 import '@shoelace-style/shoelace/dist/components/alert/alert.js';
-import '@shoelace-style/shoelace/dist/components/animation/animation.js';
 import '@shoelace-style/shoelace/dist/components/badge/badge.js';
 import '@shoelace-style/shoelace/dist/components/button/button.js';
-import '@shoelace-style/shoelace/dist/components/checkbox/checkbox.js';
 import '@shoelace-style/shoelace/dist/components/dialog/dialog.js';
 import '@shoelace-style/shoelace/dist/components/dropdown/dropdown.js';
 import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
@@ -35,7 +33,6 @@ import '@shoelace-style/shoelace/dist/components/menu/menu.js';
 import '@shoelace-style/shoelace/dist/components/popup/popup.js';
 import '@shoelace-style/shoelace/dist/components/radio/radio.js';
 import '@shoelace-style/shoelace/dist/components/switch/switch.js';
-import '@shoelace-style/shoelace/dist/components/tooltip/tooltip.js';
 
 import { setBasePath } from '@shoelace-style/shoelace/dist/utilities/base-path.js';
 setBasePath("/shoelace/");
