cc3117ea66be5079c865a69b4094cb6956ef62c6
Abraham Toriz <categulario@mail.mayfirst.org>
2026-01-29 23:02:41 -0600

    fix tests

--- chatmap-ui/tests/whatsapp.lookForMediaFile.test.js
+++ chatmap-ui/tests/whatsapp.lookForMediaFile.test.js
@@ -4,23 +4,24 @@ import { lookForMediaFile } from '../src/components/ChatMap/parsers/whatsapp';
 
 // lookForMediaFile
 
-it('should found a video', () => {
-  const filename = lookForMediaFile({
+it('should find a video', () => {
+  const { path, type } = lookForMediaFile({
     message: "‎[08/01/25, 1:48:18 p.m.] La M: ‎<adjunto: 00000005-VIDEO-2025-01-08-13-48-18.mp4>",
   });
-  expect(filename).toEqual("00000005-VIDEO-2025-01-08-13-48-18.mp4"); 
+  expect(path).toEqual("00000005-VIDEO-2025-01-08-13-48-18.mp4");
+  expect(type).toEqual("video");
 });
-it('should found a picture', () => {
-  const filename = lookForMediaFile({
+it('should find a picture', () => {
+  const { path, type } = lookForMediaFile({
     message: "‎[09/01/2025 12:23:17] Salomon: ‎< pièce jointe : 00000005-PHOTO-2025-01-09-12-23-16.jpg >",
   });
-  expect(filename).toEqual("00000005-PHOTO-2025-01-09-12-23-16.jpg"); 
+  expect(path).toEqual("00000005-PHOTO-2025-01-09-12-23-16.jpg");
+  expect(type).toEqual("image");
 });
-it('should found a picture (no space after :)', () => {
-  const filename = lookForMediaFile({
+it('should find a picture (no space after :)', () => {
+  const { path, type } = lookForMediaFile({
     message: "‎[09/01/2025 12:23:17] Salomon: ‎< pièce jointe :00000005-PHOTO-2025-01-09-12-23-16.jpg >",
   });
-  expect(filename).toEqual("00000005-PHOTO-2025-01-09-12-23-16.jpg"); 
+  expect(path).toEqual("00000005-PHOTO-2025-01-09-12-23-16.jpg");
+  expect(type).toEqual("image");
 });
-
-
