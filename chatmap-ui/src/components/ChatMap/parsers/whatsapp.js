000fd15ad36823d51dc5f830c07d527cbdf8de30
Abraham Toriz <categulario@mail.mayfirst.org>
2026-02-16 09:57:25 -0500

    use heuristics to correctly parse all dates

--- chatmap-ui/src/components/ChatMap/parsers/whatsapp.js
+++ chatmap-ui/src/components/ChatMap/parsers/whatsapp.js
@@ -18,6 +18,8 @@ const MSG_PATTERN = {
     ANDROID: /^(.*) - ([^:]*): (.*)/
 }
 
+const HOUR_PATTERN = /(\d{1,2}):(\d{1,2})(:\d{1,2})?(\s+[pP])?/;
+
 const TYPES = {
   ".jpg": "image",
   ".ogg": "audio",
@@ -104,7 +106,7 @@ const parseMessage = (line, system, getTime) => {
         }
 
         let msgObject = {
-            time: getTime(match[1]),
+            time: getTime(line),
             username:  username,
             message: match[3],
         }
@@ -132,15 +134,289 @@ export const parseTimeString = (dateStr) => {
     let dateTime = dateTimeStr.split(" ");
     const now = new Date();
     let fmtDate = [[now.getFullYear(), now.getMonth() + 1, now.getDate()].join("/"), dateTime[1]].join(" ").replaceAll(".", ":")
+
     return new Date(fmtDate);
 }
 
-function getDateFormat(lines) {
+/**
+ * Returns how many items differ between two 3-item arrays and what their
+ * indices are
+ */
+export function howManyChanged(parts1, parts2) {
+  const indices = [];
+
+  for (let i = 0; i<3; i++) {
+    if (parts1[i] !== parts2[i]) {
+      indices.push(i);
+    }
+  }
+
+  return indices;
+}
+
+/**
+ * @returns {int[]} A three-element array with the parts of a date as numbers
+ */
+function getDateParts(match) {
+  return match
+    .split(' ')[0].replace(',', '')
+    .split('/').map(Number);
+}
+
+/**
+ * @returns {string[]}
+ */
+function getTimeParts(match) {
+  const matches = match.match(HOUR_PATTERN);
+  let hour = Number(matches[1]);
+  const minute = Number(matches[2]);
+  let second = 0;
+
+  if ((matches[3] && matches[3].startsWith(' ')) || matches[4]) {
+    hour += 12;
+  } else if (matches[3] && matches[3].match(/\d+/)) {
+    second = Number(matches[3]);
+  }
+
+  return [
+    hour.toString().padStart(2, "0"),
+    minute.toString().padStart(2, "0"),
+    second.toString().padStart(2, "0"),
+  ];
+}
+
+function normalizeParts(dateParts, yearIndex, monthIndex, dayIndex, match) {
+  const year = (dateParts[yearIndex] < 100 ? dateParts[yearIndex] + 2000 : dateParts[yearIndex]).toString();
+  const [month, day] = [dateParts[monthIndex].toString().padStart(2, "0"), dateParts[dayIndex].toString().padStart(2, "0")];
+  const [hour, minute, second] = getTimeParts(match[1]);
+
+  return [year, month, day, hour, minute, second];
+}
+
+/**
+ * Returns a function that correctly parses a message's date given that no
+ * transition was found in the exported file.
+ */
+function makeCase0Function(dateParts, system) {
+  let yearIndex, monthIndex, dayIndex;
+
+  if (dateParts[0] <= 12) {
+    monthIndex = 0;
+    dayIndex = 1;
+    yearIndex = 2;
+  } else if (dateParts[1] <= 12) {
+    monthIndex = 1;
+    dayIndex = 0;
+    yearIndex = 2;
+  } else {
+    monthIndex = 2;
+    yearIndex = 0;
+    dayIndex = 1;
+  }
+
+  return (msg) => {
+    const match = msg.match(MSG_PATTERN[system]);
+    const dateParts = getDateParts(match[1]);
+    const [year, month, day, hour, minute, second] = normalizeParts(
+      dateParts, yearIndex, monthIndex, dayIndex, match
+    );
+
+    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}Z`);
+  };
+}
+
+/**
+ * returns a function that correctly parses a message's date given that a
+ * transition of a single day (no month) was found.
+ *
+ * @param {int[]} oldDate the older date
+ * @param {int[]} newDate the newer date
+ * @param {int[]} indices an array with a
+ */
+function makeCase1Function(oldDate, dayIndex, system) {
+  const otherIndex1 = (dayIndex + 1) % 3;
+  const otherIndex2 = (dayIndex + 2) % 3;
+  let yearIndex, monthIndex;
+
+  if (oldDate[otherIndex1] < oldDate[otherIndex2]) {
+    monthIndex = otherIndex1;
+    yearIndex = otherIndex2;
+  } else {
+    monthIndex = otherIndex2;
+    yearIndex = otherIndex1;
+  }
+
+  return (msg) => {
+    const match = msg.match(MSG_PATTERN[system]);
+    const dateParts = getDateParts(match[1]);
+    const [year, month, day, hour, minute, second] = normalizeParts(
+      dateParts, yearIndex, monthIndex, dayIndex, match
+    );
+
+    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}Z`);
+  };
+}
+
+/**
+ * returns a function that correctly parses a message's date given that a
+ * transition of month happened.
+ */
+function makeCase2Function(changes, maxObserved, system) {
+  // year is the index that didn't change
+  const yearIndex = changes.map((c, i) => [c, i]).filter(([c, i]) => c === 0)[0][1];
+  const otherIndex1 = (yearIndex + 1) % 3;
+  const otherIndex2 = (yearIndex + 2) % 3;
+  let  monthIndex, dayIndex;
+
+  // Day is the index that changed most, month is the other one.
+  if (changes[otherIndex1] > changes[otherIndex2]) {
+    dayIndex = otherIndex1;
+    monthIndex = otherIndex2;
+  } else if (changes[otherIndex1] == changes[otherIndex2]) {
+    if (maxObserved[otherIndex1] > maxObserved[otherIndex2]) {
+      dayIndex = otherIndex1;
+      monthIndex = otherIndex2;
+    } else {
+      dayIndex = otherIndex2;
+      monthIndex = otherIndex1;
+    }
+  } else {
+    dayIndex = otherIndex2;
+    monthIndex = otherIndex1;
+  }
+  // day is the index that became 1
+
+  return (msg) => {
+    const match = msg.match(MSG_PATTERN[system]);
+    const dateParts = getDateParts(match[1]);
+    const [year, month, day, hour, minute, second] = normalizeParts(
+      dateParts, yearIndex, monthIndex, dayIndex, match
+    );
+
+    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}Z`);
+  };
+}
+
+function makeCase3Function(changes, maxObserved, hasDecreased, system) {
+  // year index is the one that didn't decrease
+  const yearIndex = hasDecreased.map((h, i) => [h, i]).filter(([h, i]) => !h)[0][1];
+  const otherIndex1 = (yearIndex + 1) % 3;
+  const otherIndex2 = (yearIndex + 2) % 3;
+  let dayIndex, monthIndex;
+
+  const changesWithIndex = changes.map((c, i) => [c, i]);
+
+  // sort by number of changes in increasing order
+  changesWithIndex.sort((a, b) => a[0] - b[0]);
+
+  // if there's only one index bellow 12 that's the month, otherwise take the
+  // index with the most changes is the day.
+  if (maxObserved[otherIndex1] <= 12 && maxObserved[otherIndex2] <= 12) {
+    if (changes[otherIndex1] > changes[otherIndex2]) {
+      dayIndex = otherIndex1;
+      monthIndex = otherIndex2;
+    } else {
+      dayIndex = otherIndex2;
+      monthIndex = otherIndex1;
+    }
+  } else {
+    if (maxObserved[otherIndex1] <= 12) {
+      monthIndex = otherIndex1;
+      dayIndex = otherIndex2;
+    } else {
+      monthIndex = otherIndex2;
+      dayIndex = otherIndex1;
+    }
+  }
+
+  return (msg) => {
+    const match = msg.match(MSG_PATTERN[system]);
+    const dateParts = getDateParts(match[1]);
+    const [year, month, day, hour, minute, second] = normalizeParts(
+      dateParts, yearIndex, monthIndex, dayIndex, match
+    );
+
+    return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}Z`);
+  };
+}
+
+/**
+ * This function analyzes all the dates in `lines` and returns a function that
+ * correctly identifies its parts (year, month, day). It works under the
+ * assumption that the times in every line are sorted chronologically. The logic
+ * is as follows:
+ *
+ * It analyzes all messages in order and stores how many times the date changed.
+ * The core assumption is that the component that changes the most is the day,
+ * followed by the month and finally the year. Ordering the amount of changes
+ * observed we can tell what is what.
+ *
+ * @param {string[]} lines of the chat.
+ * @param {string} the identified system for this chat.
+ * @returns {function} A function that takes a message and returns correctly its
+ *                     date object.
+ */
+function getDateFormat(lines, system) {
+  let oldDateParts = [null, null, null];
+  const changes = [0, 0, 0];  // stores how many times each part has changed
+  let maxObserved = [0, 0, 0];
+  const hasDecreased = [false, false, false]
+
+  for (let line of lines) {
+    const match = line.match(MSG_PATTERN[system]);
+
+    if (match) {
+      // if this line includes a date, get its parts as numbers. E.g. "3/4/25"
+      // becomes [3, 4, 25]
+      const newDateParts = getDateParts(match[1]);
+
+      if (oldDateParts[0] === null) {
+        // this is the first date seen, set it for comparisson
+        oldDateParts = newDateParts;
+        maxObserved = newDateParts;
+      } else {
+        // see which parts have changed since the last date observed
+        const indices = howManyChanged(oldDateParts, newDateParts);
+        for (let i of indices) {
+          changes[i] += 1;
+        }
+        for (let i = 0; i < 3; i++) {
+          if (newDateParts[i] > maxObserved[i]) {
+            maxObserved[i] = newDateParts[i];
+          }
+          if (newDateParts[i] < oldDateParts[i]) {
+            hasDecreased[i] = true;
+          }
+        }
+        oldDateParts = newDateParts;
+      }
+    }
+  }
+
+  // How many date components changed?
+  const numChanges = changes.map((c) => c !== 0).reduce((a, b) => a + b, 0);
+
+  if (numChanges === 0) {
+    // nothing changed (only one date was observed in the export)
+    return makeCase0Function(oldDateParts, system);
+  } else if (numChanges === 1) {
+    // only the day ever changed.
+    const dayIndex = changes
+      .map((c, i) => [c, i])
+      .filter(([c, i]) => c > 0)[0][1];
+    return makeCase1Function(oldDateParts, dayIndex, system);
+  } else if (numChanges === 2) {
+    // date and month changed
+    return makeCase2Function(changes, maxObserved, system);
+  } else {
+    // everything changed
+    return makeCase3Function(changes, maxObserved, hasDecreased, system);
+  }
 }
 
 // Parse messages from lines and create an index
 export const parseAndIndex = (lines, system) => {
-    const getDate = getDateFormat(lines);
+    const getDate = getDateFormat(lines, system);
 
     const result = [];
     lines.forEach((line) => {
