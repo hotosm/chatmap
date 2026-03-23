f63105404af1704e94aee9518e5e673babb48139
Abraham Toriz <categulario@mail.mayfirst.org>
2026-03-23 13:39:36 -0600

    Do not use magic strings to signal no message in a point

--- chatmap-ui/src/components/Map/utils.jsx
+++ chatmap-ui/src/components/Map/utils.jsx
@@ -1,3 +1,4 @@
+import { FormattedMessage } from "react-intl";
 /*
 * Utility functions
 */
@@ -26,7 +27,7 @@ export const formatDate = (time) => {
  * @param {object} dataFiles Files with data
  * @returns {string} Message (text message or HTML for image, video)
  */
-export const getMessage = (message, msgType, dataFiles) => {
+export function GetMessage({message, msgType, dataFiles}) {
   let content;
   if (message.file && dataFiles && message.file in dataFiles) {
 
@@ -54,15 +55,17 @@ export const getMessage = (message, msgType, dataFiles) => {
   }
 
   return <>
-      {/* Media */}
-      <div className="media">
+    {/* Media */}
+    <div className="media">
       { content ? content : null }
-      </div>
-      {/* Text */}
-      { message.message && message.message !== " " && message.message !== "{location-only}" ?
-      <p className="text">{message.message}</p> : null}
-      {  message.message === "{location-only}" ?
-        <p className="text location-only">Location only</p>
-      : null}
-    </>
-}
+    </div>
+    {/* Text */}
+    { message.message && message.message !== " " ?
+      <p className="text">{message.message}</p> : (!content ?
+        <p className="text location-only">
+          <FormattedMessage id="app.map.locationOnly" defaultMessage="Location only" />
+        </p>
+      : null)
+    }
+  </>;
+};
